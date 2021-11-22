6261ecfb2e61b58f3965c4f6c1d52abf8d12e528
Carlos Garcia <carlosgarlem@gmail.com>
2021-11-22 17:36:06 -0600

    One tab solution

--- shiny_lab/server.R
+++ shiny_lab/server.R
@@ -10,65 +10,87 @@ shinyServer(function(input, output, session) {
             mutate(point_color = 'default')
     )
     
+    clicked_point <- reactiveVal(
+        ''
+    )
     
-    plotData <- reactive({
+    
+    observeEvent(input$hover_filter, {
         df <- datos()
-        hovered <- row.names(nearPoints(df, input$mouse_hover, xvar = 'wt', yvar = 'mpg'))
-        clicked <- row.names(nearPoints(df, input$clk, xvar = 'wt', yvar = 'mpg'))
-        dbclicked <- row.names(nearPoints(df, input$dclk, xvar = 'wt', yvar = 'mpg'))
-        brushed <- row.names(brushedPoints(df, input$mouse_brush, xvar = 'wt', yvar = 'mpg'))
-        
+        hovered <- row.names(nearPoints(df, input$hover_filter, xvar = 'wt', yvar = 'mpg'))
         df <- df %>%
-            mutate(point_color = ifelse(row.names(df) %in% hovered, 'hovered', point_color)) %>%
-            mutate(point_color = ifelse(row.names(df) %in% clicked, 'clicked', point_color)) %>%
-            mutate(point_color = ifelse(row.names(df) %in% dbclicked, 'default', point_color)) %>%
+            mutate(point_color = ifelse(row.names(df) %in% hovered, 'hovered', point_color))
+        
+        datos(df)
+    })
+    
+    
+    observeEvent(input$clk_filter, {
+        df <- datos()
+        clicked <- row.names(nearPoints(df, input$clk_filter, xvar = 'wt', yvar = 'mpg'))
+        df <- df %>%
+            mutate(point_color = ifelse(row.names(df) %in% clicked, 'clicked', point_color))
+        
+        datos(df)
+        clicked_point(clicked)
+    })
+    
+    
+    observeEvent(input$dclk_filter, {
+        df <- datos()
+        dbclicked <- row.names(nearPoints(df, input$dclk_filter, xvar = 'wt', yvar = 'mpg'))
+        df <- df %>%
+            mutate(point_color = ifelse(row.names(df) %in% dbclicked, 'default', point_color))
+        
+        datos(df)
+    })
+    
+    
+    observeEvent(input$brush_filter, {
+        df <- datos()
+        brushed <- row.names(brushedPoints(df, input$brush_filter, xvar = 'wt', yvar = 'mpg'))
+        df <- df %>%
             mutate(point_color = ifelse(row.names(df) %in% brushed, 'brushed', point_color))
         
         datos(df)
-        datos()
+        clicked_point(brushed)
     })
     
-    
-    output$plot_click_options <- renderPlot({
-        color_palette <- c('clicked' = 'green'
-                           , 'hovered' = 'darkgray'
-                           , 'default' = 'black'
-                           , 'brushed' = 'blue')
-        
-        
-        plotData() %>%
-            ggplot(aes(x = wt, y = mpg)) +
-            geom_point(aes(colour = point_color, size = 0.20)) +
-            scale_color_manual(values = color_palette) +
-            ylab('wt') + 
-            xlab('mpg') + 
-            ggtitle('wt vs mpg') + 
-            theme_classic()
-        
-    })
     
     
     output$plot_filter <- renderPlot({
-        mtcars %>%
-            ggplot(aes(x = wt, y = mpg)) + 
-            geom_point() + 
-            ylab('wt') + 
-            xlab('mpg') + 
-            ggtitle('wt vs mpg') + 
-            theme_minimal()
+        color_palette <- c('clicked' = 'green'
+                           , 'hovered' = 'darkgray'
+                           , 'default' = 'black'
+                           , 'brushed' = 'green')
+        
+        df <- datos()
+        isolate({
+            df %>%
+                ggplot(aes(x = wt, y = mpg)) + 
+                geom_point(aes(colour = point_color, size = 0.2)) +
+                scale_color_manual(values = color_palette) +
+                ylab('mpg') + 
+                xlab('wt') + 
+                ggtitle('wt vs mpg') + 
+                theme_minimal()
+        })                   
         
     })
     
     
     output$mtcars_tbl <- DT::renderDataTable({
-        df <- mtcars
-        clicked <- row.names(nearPoints(df, input$clk_filter, xvar = 'wt', yvar = 'mpg'))
-        brushed <- row.names(brushedPoints(df, input$brush_filter, xvar = 'wt', yvar = 'mpg'))
         
-        if((length(clicked) + length(brushed)) > 0){
-            df <- df %>%
-                filter(row.names(df) %in% c(clicked, brushed))
-        }
+        df <- mtcars
+        clicked_point()
+        isolate({
+            clicked <- row.names(nearPoints(df, input$clk_filter, xvar = 'wt', yvar = 'mpg'))
+            brushed <- row.names(brushedPoints(df, input$brush_filter, xvar = 'wt', yvar = 'mpg'))
+            if((length(clicked) + length(brushed)) > 0){
+                df <- df %>%
+                    filter(row.names(df) %in% c(clicked, brushed))
+            }
+        })
         
         df
     })
--- shiny_lab/ui.R
+++ shiny_lab/ui.R
@@ -5,25 +5,13 @@ library(DT)
 shinyUI(fluidPage(
     
     titlePanel('Shiny - Plots Interactivos'),
-    tabsetPanel(
-        tabPanel('Color Plot',
-                 plotOutput('plot_click_options',
-                            click = 'clk',
-                            dblclick = 'dclk',
-                            hover = 'mouse_hover',
-                            brush = 'mouse_brush')
-        )
-        ,
-        tabPanel('Filtrando con plots',
-                 fluidRow(
-                     column(6, 
-                            plotOutput('plot_filter',
-                                       click = 'clk_filter',
-                                       brush = 'brush_filter'),
-                     ),
-                     column(6, DT::dataTableOutput('mtcars_tbl'))
-                 )
-        )
-    )
+    fluidRow(
+        plotOutput('plot_filter',
+                   click = 'clk_filter',
+                   dblclick = 'dclk_filter',
+                   hover = hoverOpts('hover_filter', delay = 600),
+                   brush = 'brush_filter')
+    ),
+    fluidRow(DT::dataTableOutput('mtcars_tbl'))
     
 ))
